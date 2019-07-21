f919a0b6890e2b6b082cccfca61b5984b087547a
Avijit Lodh <avijitklodh@gmail.com>
2019-07-21 05:12:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,8 @@ def introduction_with_language(name,language)
 end
 
 def introduction_with_language_optional (name,language)
- if
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
- else
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+ if language.validates_length_of == 0
+  puts 
+else
+  puts "Enjoy the fine day!"
 end
